9b1b1c4f095f7db611ed1b550375dcee4afc8307
Benjamin Marasco <marascoben@gmail.com>
2021-10-26 09:51:18 -0400

    Implement boilerplate

---
+++ src/app.ts
@@ -0,0 +1,21 @@
+import 'module-alias/register';
+import express from 'express';
+import cors from 'cors';
+import { initialize } from 'express-openapi';
+import apidoc from './apidoc';
+import http2 from 'http2';
+
+const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded());
+app.use(cors());
+
+const openapi = initialize({
+    apiDoc: apidoc,
+    app,
+    paths: './dist/routes',
+    routesGlob: '**/*.{ts,js}',
+    routesIndexFileRegExp: /(?:index)?\.[tj]s$/,
+    dependencies: {}
+});
