54eed260e2f54c97214c0876dab32f37742e4a78
Benjamin Marasco <marascoben@gmail.com>
2021-10-26 09:56:00 -0400

    Add sample route

---
+++ src/routes/sample.ts
@@ -0,0 +1,14 @@
+import { NextFunction, Response } from "express";
+
+export default function (){
+    
+    const operations = {
+        get: get
+    }
+
+    async function get(req: Request, res: Response, next: NextFunction){
+		res.status(200).send();
+    }
+
+	return operations;
+}
